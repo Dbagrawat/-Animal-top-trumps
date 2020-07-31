74b955dd68d598c7737843e7b9675ba5cc994988
Dan <65185320+Dbagrawat@users.noreply.github.com>
2020-07-31 11:49:27 +0100

    Update App.js

--- src/App.js
+++ src/App.js
@@ -10,15 +10,23 @@ import Dialog from "@material-ui/core/Dialog";
 import DialogContent from "@material-ui/core/DialogContent";
 import DialogContentText from "@material-ui/core/DialogContentText";
 import DialogTitle from "@material-ui/core/DialogTitle";
+import EditAnimal from "./components/EditAnimal";
 
 const App = () => {
+    const initialFormState = {
+        id: null,
+        name: "current",
+        type: " ",
+        diet: " ",
+        isExtinct: "",
+        description: "",
+    };
+    // Setting state
+
     const [animalList, setAnimal] = useState(data);
-    const [open, setOpen] = React.useState(false);
+    const [currentAnimal, setCurrentAnimal] = useState(initialFormState);
+    const [open, setOpen] = useState(false);
     const [editing, setEditing] = useState(false);
-
-    const initialUser = { id: null, name: "", username: "" };
-
-    const [currntAnimal, setCurrentAnimal] = useState(initialUser);
 
     const handleClickOpen = () => {
         setOpen(true);
@@ -37,17 +45,19 @@ const App = () => {
     const deleteAnimal = (id) =>
         setAnimal(animalList.filter((animal) => animal.id !== id));
 
-    const editAnimal = (id, animal) => {
+    const editAnimal = (animal) => {
         setEditing(true);
+        setOpen(true);
         setCurrentAnimal(animal);
     };
 
     const updateAnimal = (newAnimal) => {
         setAnimal(
             animalList.map((animal) =>
-                animal.id === currntAnimal.id ? newAnimal : animal
+                animal.id === currentAnimal.id ? newAnimal : animal
             )
         );
+        setOpen(false);
     };
 
     return (
@@ -67,13 +77,24 @@ const App = () => {
                     onClose={handleClose}
                     aria-labelledby="form-dialog-title"
                 >
-                    <DialogTitle id="form-dialog-title">Add Animal</DialogTitle>
+                    <DialogTitle id="form-dialog-title">
+                        {editing ? "Edit Animal" : "Add Animal"}
+                    </DialogTitle>
                     <DialogContent>
                         <DialogContentText>
-                            <AddAnimal
-                                addAnimal={addAnimal}
-                                handleClose={handleClose}
-                            />
+                            {editing ? (
+                                <EditAnimal
+                                    currentAnimal={currentAnimal}
+                                    updateAnimal={updateAnimal}
+                                    //editAnimal={editAnimal}
+                                    //editing={editing}
+                                />
+                            ) : (
+                                <AddAnimal
+                                    addAnimal={addAnimal}
+                                    handleClose={handleClose}
+                                />
+                            )}
                         </DialogContentText>
                     </DialogContent>
                 </Dialog>
@@ -81,8 +102,8 @@ const App = () => {
             <AnimalCard
                 data={animalList}
                 deleteAnimal={deleteAnimal}
-                editAnimal={editAnimal}
-                handleClickOpen={handleClickOpen}
+                editThisAnimal={editAnimal}
+                //handleClickOpen={editAnimal}
             />
             <footer></footer>
         </Container>
